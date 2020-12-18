b091534e9c587f628ed8292c5e6d29fa0c56c390
cyclone77 <fillchar@live.com>
2020-12-18 17:58:19 +0800

    发布和测试环境变量保持一致

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,10 +3,10 @@ export const environment = {
     user: 'cyclone77',
     repo: 'cyclone77.github.io',
     menus: [
-        { title: 'HOME', router: '' },
-        { title: 'BLOG', router: '' },
-        { title: 'PROJECT SHOW', router: '' },
-        { title: 'ABOUT', router: '' },
-        { title: 'MESSAGE BOARD', router: '' },
+        { title: 'home', router: '' },
+        { title: 'blog', router: 'blog' },
+        { title: 'experimental project', router: 'experimental_project' },
+        { title: 'about', router: 'about' },
+        { title: 'message board', router: 'message_board' },
     ],
 };
