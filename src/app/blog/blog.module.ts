94c7b8e05dbd83fbd1ea713ef7b256723a615ad2
cyclone77 <fillchar@live.com>
2020-12-18 18:17:51 +0800

    增加字体

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { BlogComponent } from './blog.component';
+import { ArticleComponent } from './article/article.component';
 
 const routes: Routes = [
     {
         path: '',
         component: BlogComponent,
     },
+    {
+        path: 'article',
+        component: ArticleComponent,
+    },
 ];
 
 @NgModule({
-    declarations: [BlogComponent],
+    declarations: [BlogComponent, ArticleComponent],
     imports: [HttpClientModule, CommonModule, RouterModule.forChild(routes)],
 })
 export class BlogModule {}
