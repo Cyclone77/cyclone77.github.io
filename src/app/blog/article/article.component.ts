8a87453f55bbb9f88caa2e940bd2bc37a0ce6bdf
cyclone77 <fillchar@live.com>
2020-12-21 15:19:19 +0800

    markdown和代码块高亮

--- src/app/blog/article/article.component.ts
+++ src/app/blog/article/article.component.ts
@@ -1,9 +1,10 @@
 import { BlogService } from './../blog.service';
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 
-// import hljs from 'highlight.js/lib/core';
-// import typescript from 'highlight.js/lib/languages/typescript';
-// hljs.registerLanguage('typescript', typescript);
+import hljs from 'highlight.js/lib/core';
+import typescript from 'highlight.js/lib/languages/typescript';
+hljs.registerLanguage('typescript', typescript);
+import 'highlight.js/styles/github.css';
 // hljs.configure({ useBR: true });
 
 @Component({
@@ -24,6 +25,11 @@ export class ArticleComponent implements OnInit, AfterViewInit {
     loadArticleContent() {
         this.service.getArticleContent().subscribe(result => {
             this.content = result;
+            setTimeout(() => {
+                document.querySelectorAll('pre code').forEach(block => {
+                    hljs.highlightBlock(block);
+                });
+            }, 1);
         });
     }
 }
